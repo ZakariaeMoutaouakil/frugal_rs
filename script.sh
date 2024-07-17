030e92b103f4fd0d7f085c2128aff691a7e8f51a
Zakariae Moutaouakil <moutaouakilzakariae.cpgea@gmail.com>
2024-07-17 14:12:24 +0200

    Corrected script.sh

--- script.sh
+++ script.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 # Activate the virtual environment
-source "$HOME/Optimization-based-frequentist-confidence-intervals-for-functionals/venv/bin/activate"
+source "$HOME/frugal_rs/.venv/bin/activate"
 
 # Set locale to ensure decimal point handling
 export LC_NUMERIC="C"
-export PYTHONPATH="$HOME/Optimization-based-frequentist-confidence-intervals-for-functionals"
+export PYTHONPATH="$HOME/frugal_rs"
 
 sigma=0.12
+temperature=1.0
 sigma_str=$(printf "%.2f" "$sigma")
 base_classifier_path="$HOME/models/cifar10/resnet110/noise_$sigma_str/checkpoint.pth.tar"
 shift="blaise"
 
 for N in $(seq 100 10 1000)
 do
-    outfile_path="$HOME/test_results/cifar10_smoothed/noise_$sigma_str/N_$N/$shift.csv"
-    log_path="$HOME/test_results/cifar10_smoothed/noise_$sigma_str/N_$N/log_$shift.log"
+    outfile_path="$HOME/test_results/cifar10_smoothed/noise_$sigma_str/N_$N/$shift-$temperature.csv"
+    log_path="$HOME/test_results/cifar10_smoothed/noise_$sigma_str/N_$N/log_$shift-$temperature.log"
 
     # Create necessary directories
     mkdir -p "$(dirname "$outfile_path")"
@@ -23,6 +24,7 @@ do
 
     command="python certify_comparison.py\
     --base_classifier \"$base_classifier_path\" \
+    --temperature \"$temperature\" \
     --n \"$N\" \
     --shift \"$shift\" \
     --outfile \"$outfile_path\" \
